013fb222721ae1daf54b28f59d579ae6b8382370
Mugambi Javan <mugambijavan@gmail.com>
2023-08-14 13:13:44 +0300

    contact

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -74,9 +74,7 @@ const Navbar = () => {
             <li onClick={handleNav} className='p-4 text-4xl hover:text-gray-500'>
               <Link href='/work'>Work</Link>
             </li>
-            <li onClick={handleNav} className='p-4 text-4xl hover:text-gray-500'>
-              <Link href='/contact'>Contact</Link>
-            </li>
+            
           </ul>
         </div>
       </div>
